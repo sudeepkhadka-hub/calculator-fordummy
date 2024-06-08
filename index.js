82f994d4701633c0969beaec0d33fd5dec8805af
Sudeep21 <85295181+sudeepkhadka-hub@users.noreply.github.com>
2024-06-08 16:23:10 +0545

    does this commit index

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const display = document.getElementById("Display");
+const display = document.getElementById("display");
 
 function appendToDisplay(input){
 display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+
+function calculate(){
+    try{
+            display.value = eval(display.value);
+
+    }
+catch{
+    display.value = "Error";
+}
 }
